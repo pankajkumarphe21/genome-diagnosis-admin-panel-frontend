8970bb63f876591de31324e8716007d8f4ceac35
Shivam Prajapati <100841132+itsgeekhokage@users.noreply.github.com>
2024-12-29 19:57:56 +0530

    home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,45 @@
+/** @format */
+
 import React from "react";
-import STable from "../../components/Table";
+import { Container, Typography, Box, Button } from "@mui/material";
 
 export default function Home() {
-  return <div><STable/></div>;
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        textAlign: "center",
+        marginTop: 8,
+        padding: 4,
+        backgroundColor: "#f7faff",
+        borderRadius: 4,
+        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+      }}>
+      <Box sx={{ marginBottom: 4 }}>
+        <img
+          src="https://t4.ftcdn.net/jpg/08/89/42/07/360_F_889420781_UobiEdbMv2MC1VdjKnYSmKsgfY15cnwB.jpg"
+          alt="Welcome Illustration"
+          style={{ width: "100%", maxWidth: "300px", marginBottom: "20px" }}
+        />
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: "bold",
+            marginBottom: 2,
+            color: "#003366",
+          }}>
+          Welcome to The Platform
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            color: "gray",
+            fontSize: "1.1rem",
+          }}>
+
+        </Typography>
+      </Box>
+      
+    </Container>
+  );
 }
